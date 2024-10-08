21bab41330218aebd72fb14ddf7b42058fd09d20
lingkerio <lingkerio@qq.com>
2024-10-09 01:34:46 +0800

    Added elimination of epsilon productions

--- parseTreeGenerator/OCaml-version/mix.ml
+++ parseTreeGenerator/OCaml-version/mix.ml
@@ -1,14 +1,100 @@
-(* Define the grammar *)
+(* Function to eliminate epsilon productions *)
+let remove_epsilons grammar =
+  let rec remove_eps_from_rhs rhs_list eps_nonterms =
+    match rhs_list with
+    | [] -> [[]]
+    | hd :: tl ->
+      let rest = remove_eps_from_rhs tl eps_nonterms in
+      if List.mem hd eps_nonterms then
+        let new_rest = rest @ (List.map (fun r -> hd :: r) rest) in
+        new_rest
+      else
+        List.map (fun r -> hd :: r) rest
+  in
+
+  let rec find_eps_nonterms grammar =
+    grammar
+    |> List.fold_left (fun acc (lhs, rhs_list) ->
+        if List.exists ((=) ["ε"]) rhs_list then lhs :: acc else acc
+      ) []
+  in
+
+  let rec update_eps_nonterms eps_nonterms grammar =
+    let new_eps_nonterms =
+      grammar
+      |> List.fold_left (fun acc (lhs, rhs_list) ->
+          if List.exists (fun rhs -> List.for_all (fun sym -> List.mem sym eps_nonterms) rhs) rhs_list then
+            lhs :: acc
+          else
+            acc
+        ) eps_nonterms
+    in
+    if List.length new_eps_nonterms > List.length eps_nonterms then
+      update_eps_nonterms new_eps_nonterms grammar
+    else
+      new_eps_nonterms
+  in
+
+  let eps_nonterms = update_eps_nonterms (find_eps_nonterms grammar) grammar in
+
+  let new_grammar =
+    grammar
+    |> List.fold_left (fun acc (lhs, rhs_list) ->
+        let new_rhs_list =
+          rhs_list
+          |> List.fold_left (fun acc_rhs rhs ->
+              if rhs = ["ε"] then acc_rhs
+              else acc_rhs @ (remove_eps_from_rhs rhs eps_nonterms)
+            ) []
+        in
+        (lhs, new_rhs_list) :: acc
+      ) []
+  in
+
+  let rec generate_new_productions grammar eps_nonterms acc =
+    match grammar with
+    | [] -> acc
+    | (lhs, rhs_list) :: tl ->
+      let new_acc =
+        List.fold_left (fun acc rhs ->
+          if List.exists (fun sym -> List.mem sym eps_nonterms) rhs then
+            let new_rhs = List.filter (fun sym -> not (List.mem sym eps_nonterms)) rhs in
+            if new_rhs <> [] && not (List.mem (lhs, new_rhs) acc) then
+              (lhs, new_rhs) :: acc
+            else
+              acc
+          else
+            acc
+        ) acc rhs_list
+      in
+      generate_new_productions tl eps_nonterms new_acc
+  in
+
+  let new_productions = generate_new_productions new_grammar eps_nonterms [] in
+
+  (new_grammar, new_productions)
+
+(* Example grammar *)
 let grammar = [
-  ("S", [["NP"; "VP"]]);
-  ("NP", [["Det"; "N"]; ["NP"; "PP"]]);
-  ("VP", [["V"; "NP"]; ["VP"; "PP"]]);
-  ("PP", [["P"; "NP"]]);
-  ("Det", [["the"]; ["a"]]);
-  ("N", [["cat"]; ["dog"]; ["telescope"]; ["park"]]);
-  ("V", [["saw"]; ["walked"]]);
-  ("P", [["in"]; ["with"]])
+  ("E", [["L"]; ["a"]]);
+  ("L", [["("; "S"; ")"]]);
+  ("S", [["E"; "A"]]);
+  ("A", [["E"; "A"]; ["ε"]])
 ]
+
+(* Grammar and epsilon non-terminals after eliminating epsilon productions *)
+let grammar, new_productions = remove_epsilons grammar
+
+(* Output the new grammar *)
+let () =
+  Printf.printf "New grammar:\n";
+  List.iter (fun (lhs, rhs_list) ->
+    Printf.printf "%s -> %s\n" lhs (String.concat " | " (List.map (fun rhs -> String.concat " " rhs) rhs_list))
+  ) grammar;
+  Printf.printf "New productions:\n";
+  List.iter (fun (lhs, rhs) ->
+    Printf.printf "%s -> %s\n" lhs (String.concat " " rhs)
+  ) new_productions 
 
 (* Helper function to add an element to a list if it's not already present *)
 let add_unique elem lst =
@@ -32,7 +118,7 @@ let terminals =
     [] grammar
 
 (* Input sentence *)
-let sentence = "the dog saw a cat in the park"
+let sentence = "( a a a )"
 let tokens = String.split_on_char ' ' sentence
 
 (* ParseTree type to represent parse trees *)
@@ -49,18 +135,14 @@ let rec tree_to_string indent = function
         (fun acc child -> acc ^ (tree_to_string (indent ^ "  ") child)) 
         result children
 
-(* Possible splits function *)
-let rec possible_splits i j n =
-  if n = 1 then
-    if i < j then [[]] else []
-  else
-    let positions = List.init (j - i - 1) (fun k -> k + i + 1) in
-    let rec combinations acc = function
-      | 0 -> [List.rev acc]
-      | k -> 
-          List.flatten (List.map (fun pos -> combinations (pos::acc) (k-1)) positions)
-    in
-    combinations [] (n - 1)
+(* Function to convert a parse tree to typst_tree_code *)
+let rec tree_to_typst indent = function
+  | Leaf s -> Printf.sprintf "%stree(\"%s\")" indent s
+  | Node (label, children) ->
+      let new_indent = indent ^ "  " in
+      let children_typst = List.map (tree_to_typst new_indent) children in
+      let children_str = String.concat ",\n" children_typst in
+      Printf.sprintf "%stree(\"%s\",\n%s\n%s)" indent label children_str indent
 
 let rec possible_splits i j n =
   if n = 1 then
@@ -73,7 +155,7 @@ let rec possible_splits i j n =
       | _ -> 
           List.flatten (
             List.mapi (fun idx pos ->
-              let new_positions = List.filteri (fun i _ -> i <> idx) remaining_positions in
+              let new_positions = List.filteri (fun i _ -> i > idx) remaining_positions in
               let new_acc = pos :: acc in
               combinations new_acc new_positions (k-1)
             ) remaining_positions
@@ -259,9 +341,28 @@ let memo = Hashtbl.create 100
 (* Parse the sentence starting from 'S' *)
 let trees, _ = parse memo "S" 0 (List.length tokens)
 
+(* Traverse the parse tree and add the removed non-terminals *)
+let rec add_epsilons_to_tree tree new_productions =
+  match tree with
+  | Leaf _ -> tree
+  | Node (label, children) ->
+      let new_children = List.map (fun child -> add_epsilons_to_tree child new_productions) children in
+      let new_children_with_epsilons =
+        List.fold_left (fun acc (lhs, rhs) ->
+          if lhs = label && List.map (function Node (l, _) -> l | Leaf l -> l) new_children = rhs then
+            Node (lhs, new_children @ [Node (lhs, [Leaf "ε"])]) :: acc
+          else
+            acc
+        ) [Node (label, new_children)] new_productions
+      in
+      List.hd new_children_with_epsilons
+
 (* Print all possible parse trees *)
 let () = 
   List.iteri (fun idx tree ->
+    let tree_with_epsilons = add_epsilons_to_tree tree new_productions in
     Printf.printf "Parse tree %d:\n" (idx + 1);
-    print_string (tree_to_string "" tree)
+    print_string (tree_to_string "" tree_with_epsilons);
+    Printf.printf "Typst tree code %d:\n" (idx + 1);
+    print_string ("#" ^ tree_to_typst "" tree_with_epsilons)
   ) trees
